4f424d14bfeef8bf1ad5e043d51de26985b5db42
ChechuDenDauw <cecy-546@hotmail.com.ar>
2023-10-05 01:26:12 -0300

    actualizacion de cambios realizados, comienzo de seccion rutinas

---
+++ scripts/slider.js
@@ -0,0 +1,16 @@
+const slides = document.querySelectorAll('.slider-slide');
+let currentIndex = 0;
+
+function mostrarSlide(index) {
+    slides.forEach((slide, i) => {
+        slide.style.display = i === index ? 'block' : 'none';
+    });
+}
+
+function cambiarSlide() {
+    currentIndex = (currentIndex + 1) % slides.length;
+    mostrarSlide(currentIndex);
+}
+
+mostrarSlide(currentIndex);
+setInterval(cambiarSlide, 5000); 
