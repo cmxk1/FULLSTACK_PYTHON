6a1c5f92fd5e62e1918392c455504372edfd82b8
ChechuDenDauw <cecy-546@hotmail.com.ar>
2023-10-11 18:47:22 -0300

    cambios realizados modificacion de proyecto

--- scripts/slider.js
+++ scripts/slider.js
@@ -1,16 +1,16 @@
-const slides = document.querySelectorAll('.slider-slide');
-let currentIndex = 0;
+// const slides = document.querySelectorAll('.slider-slide');
+// let currentIndex = 0;
 
-function mostrarSlide(index) {
-    slides.forEach((slide, i) => {
-        slide.style.display = i === index ? 'block' : 'none';
-    });
-}
+// function mostrarSlide(index) {
+//     slides.forEach((slide, i) => {
+//         slide.style.display = i === index ? 'block' : 'none';
+//     });
+// }
 
-function cambiarSlide() {
-    currentIndex = (currentIndex + 1) % slides.length;
-    mostrarSlide(currentIndex);
-}
+// function cambiarSlide() {
+//     currentIndex = (currentIndex + 1) % slides.length;
+//     mostrarSlide(currentIndex);
+// }
 
-mostrarSlide(currentIndex);
-setInterval(cambiarSlide, 5000); 
+// mostrarSlide(currentIndex);
+// setInterval(cambiarSlide, 5000); 
